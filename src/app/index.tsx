dd867013cf133442ae9810f6d343f8d94ea13e5b
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-07 14:09:09 +0545

    props example

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import Greet from '../components/Greet'
-import CardComponent from '../components/CardComponent'
-import DynamicContent from '../components/DynamicContent'
+import React from "react";
+import Greet from "../components/Greet";
+import CardComponent from "../components/CardComponent";
+import DynamicContent from "../components/DynamicContent";
+import HugeList from "../components/HugeList";
+import ProductCard from "../components/props/ProductCard";
+import { Text } from "react-native";
+import ToggleButton from "../components/State/ToggleButton";
+import WeatherApp from "../components/State/WeatherApp";
+import ColorPicker from "../components/State/ColorPicker";
+import TodoList from "../components/State/TodoList";
 
 const index = () => {
   return (
-    <DynamicContent/>
-  )
-}
+    <>
+      {/* <ProductCard /> */}
+      {/* <ToggleButton/> */}
+      {/* <WeatherApp/> */}
+      {/* <ColorPicker/> */}
+      <TodoList/>
+    </>
+  );
+};
 
-
-
-export default index
+export default index;
